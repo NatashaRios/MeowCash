e571eeed4043928ab5435c67d4ca24e6ebee3d32
Natasha <riosnatasha94@gmail.com>
2024-07-11 23:05:51 -0300

    Add fonts

---
+++ src/constants/fonts.ts
@@ -0,0 +1,8 @@
+export const fontFamily = {
+  light: 'NotoSans-Light',
+  regular: 'NotoSans-Regular',
+  semiBold: 'NotoSans-SemiBold',
+  bold: 'NotoSans-Bold',
+  extraBold: 'NotoSans-ExtraBold',
+  black: 'NotoSans-Black',
+};
