606b80c43ddae02c1bef59f55311d0ff387a7025
Natasha <riosnatasha94@gmail.com>
2024-07-12 15:25:39 -0300

    Add splash and app icon in ios

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigation } from './src/navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <RootNavigation />
