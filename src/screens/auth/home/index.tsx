08cb958b3b3a6452a55b53f3c07762c2e00a6797
Natasha <riosnatasha94@gmail.com>
2024-07-14 01:30:24 -0300

    Add searcher

--- src/screens/auth/home/index.tsx
+++ src/screens/auth/home/index.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from 'react';
-import { ActivityIndicator, FlatList, SafeAreaView, View } from 'react-native';
+import React, { FC, useState } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  TextInput,
+  View,
+} from 'react-native';
 import { styles } from './styles';
 import { useGetCryptoListingLatest } from '@/services/hooks/coinMarketCap/useGetCryptoListingLatest';
 import { CryptoInformation, Heading2 } from '@/components';
 import { useTranslation } from 'react-i18next';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamsList } from '@/navigation';
+import { colors } from '@/constants/colors';
+import { CryptoListingLatest } from '@/interfaces/cryptoListingLatest';
 
 type NavigationProps = NativeStackScreenProps<RootStackParamsList, 'Home'>;
 
@@ -13,9 +21,24 @@ export const Home: FC<NavigationProps> = ({ navigation }) => {
   const { data, isLoading, isError } = useGetCryptoListingLatest();
   const { t } = useTranslation();
 
+  const [filteredData, setFilteredData] = useState<CryptoListingLatest[]>();
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = (text: string) => {
+    const lowerCaseQuery = text.toLowerCase();
+    const filtered = data?.data.filter(
+      item =>
+        item.name.toLowerCase().includes(lowerCaseQuery) ||
+        item.symbol.toLowerCase().includes(lowerCaseQuery),
+    );
+    setFilteredData(filtered);
+    setSearchText(text);
+  };
+
   const handleCryptoPress = (id: number): void => {
     navigation.navigate('DetailCrypto', { id });
   };
+
   isLoading && <ActivityIndicator />;
 
   return (
@@ -23,8 +46,17 @@ export const Home: FC<NavigationProps> = ({ navigation }) => {
       <View style={styles.topContent}>
         <Heading2 text={t('home.title')} accessibilityLabel={t('home.title')} />
       </View>
+      <TextInput
+        style={styles.input}
+        placeholder={t('home.search')}
+        placeholderTextColor={colors.pink}
+        value={searchText}
+        onChangeText={handleSearch}
+        accessibilityRole="search"
+        accessible={true}
+      />
       <FlatList
-        data={data?.data}
+        data={searchText.length > 0 ? filteredData : data?.data || []}
         keyExtractor={item => item.id.toString()}
         renderItem={({ item: { id, name, symbol, quote } }) => (
           <CryptoInformation
--- src/screens/auth/home/styles.ts
+++ src/screens/auth/home/styles.ts
@@ -12,4 +12,18 @@ export const styles = StyleSheet.create({
     padding: 10,
     height: 50,
   },
+  input: {
+    height: 40,
+    color: colors.pink,
+    marginHorizontal: 15,
+    marginVertical: 12,
+    shadowColor: colors.veryLightPink,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 5,
+    backgroundColor: colors.brownBurgundyDark,
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
 });
